8de7a88329d98e4ae03f804b8a95031e7a58f504
Bruno Geraldo <bruno_grld@hotmail.com>
2020-05-15 22:44:28 -0300

    [dev] cadastrando usuário de criptografando a senha

--- src/main/java/dao/UsuarioDao.java
+++ src/main/java/dao/UsuarioDao.java
@@ -1,41 +1,58 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
 import entities.Usuario;
-
-
+import util.ConnectionDBUtil;
+import util.HashUtil;
 
 public class UsuarioDao {
 
 	private static UsuarioDao INSTANCE = new UsuarioDao();
 
-	//private ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+	// private ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
 
 	public static UsuarioDao getInstance() {
 		return INSTANCE;
 	}
 
-	/*public ArrayList<Usuario> getUsuarios() {
-		return this.usuarios;
-	}*/
+	/*
+	 * public ArrayList<Usuario> getUsuarios() { return this.usuarios; }
+	 */
 
 	public boolean create(Usuario usuario) {
+		boolean status=false;
+		try {
+			Connection conn = ConnectionDBUtil.gecConnection();
 
-		boolean status = false;
+			String sql = "INSERT INTO USUARIO (CPF,NOME,EMAIL,SEXO,SENHA) VALUES (?,?,?,?,?)";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, usuario.getCpf());
+			ps.setString(2, usuario.getNome());
+			ps.setString(3, usuario.getEmail());
+			ps.setString(4, usuario.getSexo());
+			ps.setString(5, HashUtil.getHash(usuario.getSenha()));
+
+			ps.execute();
+			status = true;
+		} catch (Exception e){
+			System.out.println("Error: "+e.getMessage());
+		 status = false;
+		}
 
 		return status;
 	}
 
 	public boolean login(String cpf, String senha) {
-     return true;
+		return true;
 	}
 
 	public boolean sinbleField(String type, String field) {
 		String sql = "";
 		boolean status = true;
-
 
 		return status;
 	}
--- src/main/java/util/ConnectionDBUtil.java
+++ src/main/java/util/ConnectionDBUtil.java
@@ -1,25 +1,23 @@
 package util;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class TesteDB {
+public class ConnectionDBUtil {
 
-	public static void main(String[] args) {
+	public static Connection gecConnection() {
+		Connection conn = null;
 		try {
 			String sqlDriver = "oracle.jdbc.OracleDriver";
 			String url = "jdbc:oracle:thin:@localhost:1521:XE";
-			String user = "bruno2";
+			String user = "bruno3";
 			String password = "123456";
 
 			Class.forName(sqlDriver);
-			Connection conn = DriverManager.getConnection(url, user, password);
-			System.out.println(conn);
-			conn.close();
-			// return conn;
+			conn = DriverManager.getConnection(url, user, password);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
-			// return null;
 		}
+		return conn;
 	}
-
 }
